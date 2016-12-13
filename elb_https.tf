411ed6f7836e29dcd134029943d70efe7b2c4e84
John Patterson <notarobot@nextrevision.net>
2016-12-13 08:57:32 -0500

    Adding initial support for ALB

--- elb_https.tf
+++ elb_https.tf
@@ -1,48 +1,53 @@
-#------------------------------------------#
-# Elastic Load Balancer HTTPS Configuration
-#------------------------------------------#
-resource "aws_elb" "rancher_ha_https" {
-    count                       = "${var.enable_https}"
-    name                        = "${var.name_prefix}-elb-https"
-    internal                    = false
-    idle_timeout                = 60
-    connection_draining         = true
-    connection_draining_timeout = 30
-    cross_zone_load_balancing   = true
+#----------------------------------------------#
+# Application Load Balancer HTTPS Configuration
+#----------------------------------------------#
+resource "aws_alb" "rancher_ha" {
+    count                      = "${var.enable_https}"
+    name                       = "${var.name_prefix}-alb-https"
+    internal                   = false
+    enable_deletion_protection = true
 
-    instances = ["${aws_instance.rancher_ha.*.id}"]
     subnets = ["${aws_subnet.rancher_ha.*.id}"]
 
     security_groups = [
         "${aws_security_group.rancher_ha_elb.id}",
-        "${aws_security_group.rancher_ha_elb_https.id}"
+        "${aws_security_group.rancher_ha_alb_https.id}"
     ]
+}
 
-    listener {
-        instance_port = 8080
-        instance_protocol = "tcp"
-        lb_port = 443
-        lb_protocol = "ssl"
-        ssl_certificate_id = "${aws_iam_server_certificate.rancher_ha.arn}"
-    }
+resource "aws_alb_listener" "rancher_ha" {
+    count             = "${var.enable_https}"
+    load_balancer_arn = "${aws_alb.rancher_ha.arn}"
+    port              = "443"
+    protocol          = "HTTPS"
+    ssl_policy        = "ELBSecurityPolicy-2015-05"
+    certificate_arn   = "${aws_iam_server_certificate.rancher_ha.arn}"
 
-    health_check {
-        healthy_threshold = 2
-        unhealthy_threshold = 2
-        timeout = 3
-        target = "HTTP:8080/ping"
-        interval = 30
-    }
-
-    tags {
-        Name = "${var.name_prefix}-elb-https"
+    default_action {
+        target_group_arn = "${aws_alb_target_group.rancher_ha.arn}"
+        type             = "forward"
     }
 }
 
-resource "aws_proxy_protocol_policy" "https" {
-    count          = "${var.enable_https}"
-    load_balancer  = "${aws_elb.rancher_ha_https.name}"
-    instance_ports = ["443"]
+resource "aws_alb_target_group" "rancher_ha" {
+    count    = "${var.enable_https}"
+    name     = "${var.name_prefix}-target-group"
+    port     = 8080
+    protocol = "HTTP"
+    vpc_id   = "${aws_vpc.rancher_ha.id}"
+
+    health_check {
+        path     = "/ping"
+        protocol = "HTTP"
+    }
+}
+
+# TODO: figure out how to optionally enable this attachment based on var.enable_https
+resource "aws_alb_target_group_attachment" "rancher_ha" {
+    count            = "${length(aws_instance.rancher_ha.*.id)}"
+    target_group_arn = "${aws_alb_target_group.rancher_ha.arn}"
+    target_id        = "${element(aws_instance.rancher_ha_a.*.id, count.index)}"
+    port             = 8080
 }
 
 resource "aws_iam_server_certificate" "rancher_ha" {
@@ -53,9 +58,9 @@ resource "aws_iam_server_certificate" "rancher_ha" {
     certificate_chain = "${file("${var.cert_chain}")}"
 }
 
-resource "aws_security_group" "rancher_ha_elb_https" {
+resource "aws_security_group" "rancher_ha_alb_https" {
     count       = "${var.enable_https}"
-    name        = "${var.name_prefix}-elb-https"
+    name        = "${var.name_prefix}-alb-https"
     description = "Rancher HA Public HTTPS Traffic"
     vpc_id      = "${aws_vpc.rancher_ha.id}"
 
